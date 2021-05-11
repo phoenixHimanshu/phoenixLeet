d57a03aec6fa9a6a32b0d143c660ab2e514dba08
hbhange <himanshu.bhange@gmail.com>
2021-05-11 11:26:31 +0530

    Added Comments to understand DFS and BFS implementation

--- Graph/graph.go
+++ Graph/graph.go
@@ -37,9 +37,14 @@ func getVerticesEdgesMatrix() [7][7]int {
 
 }
 
+//=================== DFS===============
+// checking visited node otherwise it will end up in
+// an infinite loop between 1 - 0 and 0 - 1
+
 func printGraphDFS(adjMatrix [7][7]int, StartVertex int) {
 	v := len(adjMatrix)
 	visited := make([]bool, v)
+	//This loop is to check edge like 5-6
 	for i := 0; i < v; i++ {
 		if visited[i] == false {
 			printDfsHelper(adjMatrix, visited, StartVertex)
@@ -61,10 +66,12 @@ func printDfsHelper(adjMat [7][7]int, visit []bool, sv int) {
 }
 
 //=================== BFS===============
-
+//The key thing to remember in BFS is the useage of queue
+// Iterative solution with usage of stack works best for BFS
 func printGraphBFS(adjMatrix [7][7]int) {
 	v := len(adjMatrix)
 	visited := make([]bool, v)
+	//This loop ensures working on edge between vertices 5-6
 	for i := 0; i < v; i++ {
 		if visited[i] == false {
 			printBfsHelper(adjMatrix, visited, i)
@@ -77,17 +84,18 @@ func printBfsHelper(adjmatrix [7][7]int, visited []bool, sv int) {
 	queue.PushBack(sv)
 	visited[sv] = true
 	numOfVertex := len(adjmatrix)
-
+	//The outer loop below is to traverse through queue
+	// ex 0-4 and 4-0 has like , 0 is starting vertex
+	// so queue in first iteration would have = 1 ,4 = inside queue
 	for 0 < queue.Len() {
-		//polling queue
+		//polling queue ie take out the first element from queue
 		front := queue.Front().Value.(int)
 		println(front)
 		queue.Remove(queue.Front())
-
+		//This inner loop is for checking unvisited edges between
+		// vertices
 		for i := 0; numOfVertex > i; i++ {
-			//queue.PushBack(i)
 			if adjmatrix[front][i] == 1 && visited[i] == false {
-				//println(i)
 				queue.PushBack(i)
 				visited[i] = true
 			}
--- Graph/graph_test.go
+++ Graph/graph_test.go
@@ -9,6 +9,10 @@ import "testing"
        1 	 4
 		\	 /
 		2 -  3
+					5-6
+Note: vertex 5 and 6 have and edge and this is disconnected
+graph
+
 */
 
 func TestGetVerticesEdges(t *testing.T) {
